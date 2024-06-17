8934c2fd26efc1dd6c95c374e541ec3224b20e6d
MironovErnest <erik1297e@gmail.com>
2024-06-17 12:05:10 +0300

    chore: Update Analytics component to include LineChartModule and SankeyDiagramBasicDemo

--- src/pages/analytics/Analytics.tsx
+++ src/pages/analytics/Analytics.tsx
@@ -1,7 +1,17 @@
+import { SankeyDiagramBasicDemo } from '@components/chart/sankey/SankeyD3'
+import { LineChartModule } from '@modules/charts/LineChartModule'
 import type { ComponentProps } from 'react'
 
 interface AnalyticsProperties extends ComponentProps<'div'> {}
 
 export const Analytics = (_props: AnalyticsProperties) => {
-  return <div>Analytics</div>
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-4">
+        <LineChartModule title="APY" />
+        <LineChartModule title="TVL" />
+      </div>
+      <SankeyDiagramBasicDemo />
+    </div>
+  )
 }
