37ce5ca58e944de28d470c3673650197356c64c5
Vylegzhanin Ilya <vylegzhanin.ilja@yandex.ru>
2024-06-18 19:59:09 +0300

    fix: lock

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,7 +2,7 @@ interface LogoProperties extends React.SVGProps<SVGSVGElement> {
   fill?: string
 }
 
-export const Logo = ({ fill }: LogoProperties) => {
+export const Logo = ({ fill, ...rest }: LogoProperties) => {
   return (
     <svg
       width="67"
