9757da975c0a3bec880ba094acf1ef29aca709d0
MironovErnest <erik1297e@gmail.com>
2024-06-19 10:49:12 +0300

    chore: Remove unused code and dependencies in Strategies module

--- src/pages/analytics/modules/Strategies.tsx
+++ src/pages/analytics/modules/Strategies.tsx
@@ -1,14 +1,7 @@
 import { Button } from '@components/ui/button'
 import { Logo } from '@components/ui/logo'
-import {
-  createColumnHelper,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from '@tanstack/react-table'
 import { cn } from '@utils/cn'
 import type { ReactNode } from 'react'
-import { useState } from 'react'
 
 type Person = {
   Token: ReactNode
@@ -42,36 +35,7 @@ const defaultData: Person[] = [
   },
 ]
 
-const columnHelper = createColumnHelper<Person>()
-
-const columns = [
-  columnHelper.accessor('Token', {
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor('Chain', {
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor('Protocol', {
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor('ProjectedAPY', {
-    header: 'Projected APY',
-    cell: (info) => info.getValue(),
-  }),
-  columnHelper.accessor('TVL', {
-    cell: (info) => info.getValue(),
-  }),
-]
-
 export const Strategies: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
-  const [data] = useState(() => [...defaultData])
-
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
-
   return (
     <div {...props} className={cn('flex flex-col gap-[1.5rem]', props.className)}>
       <div className="flex items-center justify-between">
@@ -81,32 +45,6 @@ export const Strategies: React.FC<React.HTMLAttributes<HTMLDivElement>> = (props
         </h2>
         <Button className="mt-4">DEPOSIT</Button>
       </div>
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.header, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   )
 }
