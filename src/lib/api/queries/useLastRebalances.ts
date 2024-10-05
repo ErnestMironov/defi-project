875cd9685e6ccbe0a964a44536f4c0aacba18f9c
0x1luxa <0x1luxa@gmail.com>
2024-10-05 18:42:24 +0700

    feat: rebalance chart

---
+++ src/lib/api/queries/useLastRebalances.ts
@@ -0,0 +1,20 @@
+import { apiClient } from '@api/maat-finance/api-client'
+import type { Event } from '@api/maat-finance/types'
+import { useQuery } from '@tanstack/react-query'
+
+export type LastRebalancesType = Record<string, Event[]>
+
+export const getLastRebalances = () => {
+  return apiClient.get<LastRebalancesType>('/actions/last/rebalances')
+}
+
+export const useLastRebalances = () => {
+  return useQuery({
+    queryKey: ['last-rebalances'],
+    queryFn: async () => {
+      const { data } = await getLastRebalances()
+
+      return data
+    },
+  })
+}
--- src/pages/analytics/Analytics.tsx
+++ src/pages/analytics/Analytics.tsx
@@ -1,3 +1,4 @@
+import { RebalanceChart } from '@components/chart/sankey/SankeyD3'
 import { Footer } from '@layouts/footer/Footer'
 import { Strategies } from '@modules/strategies/Strategies'
 import { TokenOverview } from '@modules/token-overview/TokenOverview'
@@ -10,7 +11,7 @@ export const Analytics = () => {
       <TokenCharts className="mt-[5.31rem] max-lg:mt-6" />
       <TokenOverview className="mt-12 max-lg:mt-8" />
       <Strategies withLink className="mt-[6.25rem]" />
-      {/* <SankeyDiagramBasicDemo className="mt-[6.25rem]" /> */}
+      <RebalanceChart className="mt-[6.25rem]" />
       <Transactions
         className="mt-28 max-lg:mt-14"
         parameters={{ transaction_type: 'trigger' }}
