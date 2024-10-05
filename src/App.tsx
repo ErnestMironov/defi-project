2dcfe9667c3db4046b2f3f41c1a6b9ba89568805
0x1luxa <0x1luxa@gmail.com>
2024-10-05 19:02:45 +0700

    feat: remove graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/default-param-last */
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
-import { relayStylePagination } from '@apollo/client/utilities'
 import { queryClient } from '@configs/r-query'
 import { Web3ModalProvider } from '@configs/Web3ModalProvider'
 import { PendingTransactions } from '@modules/pending-transactions/PendingTransactions'
@@ -11,38 +9,17 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
 import { RouterProvider } from 'react-router-dom'
 
-const GRAPHQL_URL = 'https://lumos-labs.squids.live/maat/v/v1/graphql'
-
-const client = new ApolloClient({
-  uri: GRAPHQL_URL,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          maatActionsConnection: relayStylePagination([
-            ['where', ['token']],
-            'first',
-            // 'after',
-          ]),
-        },
-      },
-    },
-  }),
-})
-
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <ApolloProvider client={client}>
-          <Web3ModalProvider>
-            <QueryClientProvider client={queryClient}>
-              <RouterProvider router={router} />
-              <PendingTransactions />
-              <TxReviewModal />
-            </QueryClientProvider>
-          </Web3ModalProvider>
-        </ApolloProvider>
+        <Web3ModalProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <PendingTransactions />
+            <TxReviewModal />
+          </QueryClientProvider>
+        </Web3ModalProvider>
       </ThemeProvider>
       <Toaster />
     </>
