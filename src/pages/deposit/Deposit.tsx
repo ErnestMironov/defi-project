bbf90ccd21c6870af842931a31d52886431c1846
MironovErnest <erik1297e@gmail.com>
2024-11-14 12:58:13 +0700

    feat: add terms and policy

--- src/pages/deposit/Deposit.tsx
+++ src/pages/deposit/Deposit.tsx
@@ -39,7 +39,7 @@ export const Deposit = () => {
   }, [bestUSDCAPy, bestUSDTAPy, setVault, isStrategiesLoading])
 
   return (
-    <div className="flex w-full flex-col items-center justify-between gap-10 pb-8">
+    <div className="flex h-screen w-full flex-col items-center justify-between gap-10 pb-10">
       <div className="pointer-events-auto mt-10 flex flex-col gap-6 max-lg:mt-8 max-lg:gap-4 lg:w-[38.75rem]">
         {isBelowDesktop && <TVLDisplay />}
 
@@ -73,6 +73,30 @@ export const Deposit = () => {
           )}
         </div>
         <TransactionBlock />
+      </div>
+      <div className="grid w-full justify-between gap-4 pb-10 max-lg:mt-20 lg:grid-cols-[1fr,2fr,1fr]">
+        <div className="" />
+        <p className="max-w-full text-center text-xs leading-[120%] text-[#c4c4c4] dark:text-text-50 lg:max-w-[56.25rem]">
+          Cryptocurrencies and decentralized finance (DeFi) carry significant risk,
+          including market volatility, smart contract vulnerabilities, and potential loss
+          of funds. MAAT does not provide financial, legal, or tax advice. Users are
+          solely responsible for conducting their own research and understanding the risks
+          before interacting with the platform.
+        </p>
+        <div className="flex gap-2 justify-self-center text-center text-xs leading-[120%] text-[#c4c4c4] dark:text-text-50 lg:max-w-[56.25rem] lg:justify-self-end">
+          <a
+            href="https://docs.maat.finance/legal/terms-of-service"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-main-100"
+          >
+            Terms
+          </a>
+          |
+          <a href="" target="_blank" rel="noreferrer" className="hover:text-main-100">
+            Policy
+          </a>
+        </div>
       </div>
     </div>
   )
