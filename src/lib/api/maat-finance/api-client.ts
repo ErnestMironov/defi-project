7bdc773242dd30705063fab7e4cfccb89b267f31
0x1luxa <0x1luxa@gmail.com>
2024-11-20 10:18:37 +0300

    fix: change url

--- src/lib/api/maat-finance/api-client.ts
+++ src/lib/api/maat-finance/api-client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // const BASE_URL = 'https://api.maat.finance'
-// const BASE_URL = 'https://dev-api.maat.finance'
-export const BASE_URL = 'https://api.maat.finance'
+export const BASE_URL = 'https://dev-api.maat.finance'
+// export const BASE_URL = 'https://api.maat.finance'
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
