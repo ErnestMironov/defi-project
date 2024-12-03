4ca78575ec30b7e22e0517514d6f05267be23060
Mikhail <dukin.misha22@gmail.com>
2024-12-03 22:40:02 +0300

    update modal state

--- src/layouts/header/hooks/UsePortfolioModalState.tsx
+++ src/layouts/header/hooks/UsePortfolioModalState.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 export const usePortfolioModalState = () => {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   const handlePortfolioOpen = () => {
     setIsOpen(!isOpen)
