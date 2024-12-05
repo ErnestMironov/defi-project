b41786e640c02124dc41dea66f4cd67061541277
0xSeppuk <0xseppuk@gmail.com>
2024-12-05 17:05:14 +0300

    changed currency calculations

--- src/modules/portfolio/components/YieldPotential.tsx
+++ src/modules/portfolio/components/YieldPotential.tsx
@@ -10,7 +10,9 @@ export function YieldPotential({ potentialUsdProfit }: YieldPotentialProperties)
     <div className="mt-4 px-6">
       <div className="h-auto w-full rounded-lg bg-light-blue-15 px-6 py-3 text-center text-white">
         <span className="text-main-100 opacity-70">Yield Potential</span>{' '}
-        <span className="text-main-100">{formatUsdValue(potentialUsdProfit)}/year</span>
+        <span className="text-main-100">
+          {formatUsdValue(potentialUsdProfit, { notation: 'compact' })}/year
+        </span>
       </div>
     </div>
   )
