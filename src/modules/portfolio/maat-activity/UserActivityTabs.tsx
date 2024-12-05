16d8a2c591cb40574c25193501153168efbe7500
0xSeppuk <0xseppuk@gmail.com>
2024-12-05 09:48:31 +0300

    change structure

--- src/modules/portfolio/maat-activity/UserActivityTabs.tsx
+++ src/modules/portfolio/maat-activity/UserActivityTabs.tsx
@@ -16,29 +16,27 @@ export const UserActivityTabs = (props: UserActivityTabsProperties) => {
   return (
     <div className={cn('flex flex-col gap-4', className)} {...rest}>
       <Tabs defaultValue="tokens" className="" onValueChange={setActiveTab}>
-        {value > 0 && (
-          <TabsList className=" flex w-full justify-between border-b  px-6">
-            <div className="flex gap-4">
-              <TabsTrigger
-                variant="unstyled"
-                value="tokens"
-                className="py-3 text-md data-[state='active']:border-b-2 data-[state='active']:border-b-main-100 data-[state='active']:text-main-100"
-                disabled={value <= 0}
-              >
-                Assets
-              </TabsTrigger>
-              <TabsTrigger
-                value="activity"
-                variant="unstyled"
-                className="py-3 text-md data-[state='active']:border-b-2 data-[state='active']:border-b-main-100 data-[state='active']:text-main-100"
-                disabled={value <= 0}
-              >
-                Activity
-              </TabsTrigger>
-            </div>
-            {activeTab === 'activity' && <a href="##">See all</a>}
-          </TabsList>
-        )}
+        <TabsList className=" flex w-full justify-between border-b  px-6">
+          <div className="flex gap-4">
+            <TabsTrigger
+              variant="unstyled"
+              value="tokens"
+              className="py-3 text-md data-[state='active']:border-b-2 data-[state='active']:border-b-main-100 data-[state='active']:text-main-100"
+              disabled={value <= 0}
+            >
+              Assets
+            </TabsTrigger>
+            <TabsTrigger
+              value="activity"
+              variant="unstyled"
+              className="py-3 text-md data-[state='active']:border-b-2 data-[state='active']:border-b-main-100 data-[state='active']:text-main-100"
+              disabled={value <= 0}
+            >
+              Activity
+            </TabsTrigger>
+          </div>
+          {activeTab === 'activity' && <a href="##">See all</a>}
+        </TabsList>
         <TabsContent value="tokens">
           <UserTokens />
         </TabsContent>
