72864dd27121fd59a6997b84c4e9d5d3e69b58f0
0xSeppuk <0xseppuk@gmail.com>
2024-12-05 09:37:18 +0300

    replaced pixel values with rems and added background circle

--- src/modules/portfolio/all-assets/NoDeposit.tsx
+++ src/modules/portfolio/all-assets/NoDeposit.tsx
@@ -1,3 +1,4 @@
+import BackgroundCircles from '@assets/icons/backgroundCircles.svg'
 import NoDepositImage from '@assets/images/NoDeposit.png'
 import { Button } from '@components/ui/button'
 import { useTxStore } from '@modules/transaction-block/store/useTxStore'
@@ -11,17 +12,18 @@ interface NoDepositProperties extends ComponentProps<'div'> {}
 export const NoDeposit = (props: NoDepositProperties) => {
   const { setTxType } = useTxStore()
   const { className, ...rest } = props
+
   return (
     <div
       className={cn(
-        'flex flex-col gap-[16px] items-center justify-between flex-row border-b  pl-6',
+        'flex flex-col gap-[16px] items-center justify-between flex-row border-b pl-6',
         className,
       )}
       {...rest}
     >
-      <div className="">
-        <h2 className="text-base-100  text-lg font-semibold">No deposits?...</h2>
-        <p className="mt-1 text-text-2100 ">
+      <div className="max-w-[11.875rem]">
+        <h2 className="text-base-100 text-base font-medium">No deposits?...</h2>
+        <p className="mt-1 text-xs font-normal leading-4 text-text-2100">
           Try out MAAT and get 10% return on USDC and USDT allocations.
         </p>
         <Button
@@ -33,12 +35,14 @@ export const NoDeposit = (props: NoDepositProperties) => {
           Deposit now
         </Button>
       </div>
-      <div className="relative flex items-center justify-center">
-        {' '}
-        {/* <Circle className="absolute z-0 size-12" />
-        <Circle className="absolute z-0 size-60" />
-        <Circle className="size-70 absolute z-0" /> */}
-        <img src={NoDepositImage} alt="" className="z-10 max-w-[190px]" />
+
+      <div className="relative flex items-center justify-end overflow-hidden">
+        <BackgroundCircles className=" absolute bottom-0 right-0 border border-text-30100 opacity-10" />
+        <img
+          src={NoDepositImage}
+          alt="No Deposit"
+          className="relative  max-w-[11.875rem]"
+        />
       </div>
     </div>
   )
