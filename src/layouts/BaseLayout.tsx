b8bd68ee36e77b18c932e27f397c0a227cabc77b
Mikhail <dukin.misha22@gmail.com>
2024-12-14 21:48:34 +0300

    change background

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,17 +1,11 @@
-import { useTheme } from '@modules/theme/ThemeProvider'
-import { ROUTES } from '@routes/routes'
 import { type ComponentProps } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import { Header } from './header/Header'
 
 interface BaseLayoutProperties extends ComponentProps<'div'> {}
 
 export const BaseLayout = (_props: BaseLayoutProperties) => {
-  const { theme } = useTheme()
-  const { pathname } = useLocation()
-  const customBgPage = [ROUTES.PORTFOLIO, ROUTES.POINTS].includes(pathname as never)
-
   return (
     <div className="flex min-h-screen flex-col px-[6.25rem] max-lg:px-4 ">
       <Header className="mt-4 lg:mt-[2.06rem]" />
