6bc9f4a5b5ed09049899b25869b7b8395d4f17c1
0xSeppuk <0xseppuk@gmail.com>
2024-12-25 15:25:02 +0300

    fix ui bugs

--- src/modules/portfolio/maat-activity/UserTransactionItem.tsx
+++ src/modules/portfolio/maat-activity/UserTransactionItem.tsx
@@ -1,4 +1,5 @@
 import type { Event } from '@api/maat-finance/types'
+import ArrowTopRightIcon from '@assets/icons/bordered-arrow-top-right.svg'
 import LinkIcon from '@assets/icons/externalLinkIcon.svg'
 import MoreOptionsIcon from '@assets/icons/more-options.svg'
 import TransactionDetails from '@assets/icons/TransactionDetails.svg'
@@ -37,15 +38,15 @@ const DropdownMenuForPortfolio: React.FC<UserTransactionItemProperties> = (props
   const isMobile = window.matchMedia('(max-width: 768px)').matches
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger className="rounded-xl border p-2 text-gray-700 hover:bg-gray-200">
+      <DropdownMenu.Trigger className="rounded-xl border border-stroke-100 p-2 text-gray-700 hover:bg-gray-200">
         <MoreOptionsIcon className="size-full" />
       </DropdownMenu.Trigger>
       <DropdownMenu.Content
-        className="mr-2 mt-4 w-[15.75rem] gap-1 rounded-xl border border-stroke-100 bg-cards p-1 shadow-test-2"
+        className="mr-2 mt-4 w-[15.75rem] gap-1 rounded-xl border border-stroke-100 bg-cards-widget p-1 shadow-test-2"
         side="left"
         align="end"
       >
-        <DropdownMenu.Item className="cursor-pointer rounded-xl bg-cards p-4 hover:bg-light-blue-15">
+        <DropdownMenu.Item className="cursor-pointer rounded-xl bg-cards-widget p-4 hover:bg-light-blue-15">
           <Link
             to={`${ROUTES.TRANSACTIONS}/${event.hash}`}
             className="flex w-full items-center gap-4"
@@ -56,13 +57,20 @@ const DropdownMenuForPortfolio: React.FC<UserTransactionItemProperties> = (props
           </Link>
         </DropdownMenu.Item>
         <DropdownMenu.Item className="cursor-pointer rounded-xl p-4 hover:bg-light-blue-15">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-4">
-              <LinkIcon className="size-5" />
-              <p className="ml-1 text-sm">View on scanner</p>
+          <ScanLink
+            chainId={event.src_chain_id}
+            txHash={event.hash}
+            className="block"
+            arrow={false}
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-4">
+                <LinkIcon className="size-5" />
+                <p className="ml-1 text-sm">View on scanner</p>
+              </div>
+              <ArrowTopRightIcon className="size-4" />
             </div>
-            <ScanLink chainId={event.src_chain_id} txHash={event.hash} className="ml-1" />
-          </div>
+          </ScanLink>
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
