0eafc523b6dddd7887798d6e9cc27e8fad673c3a
Mikhail <dukin.misha22@gmail.com>
2025-01-05 21:15:34 +0300

    round points balance

--- src/modules/points-balance/PointsBalance.tsx
+++ src/modules/points-balance/PointsBalance.tsx
@@ -7,24 +7,26 @@ import { useAccount } from 'wagmi'
 
 export const PointsBalance = ({ className }: { className?: string }) => {
   const { address } = useAccount()
-  const { data: badgesInfo } = useGetUserBadges(address as Address)
+  const { data: badgesInfo } = useGetUserBadges(
+    '0x5a3273e64d0C36379EC4a826DF2c9a3092683928' as Address,
+  )
 
   return (
     <div
       className={cn(
-        'flex h-[3rem]  rounded-[1rem] bg-white border border-stroke-100 dark:bg-cards-widget dark:border-stroke-40100',
+        'flex h-[3rem] max-md:h-[2.5rem] max-md:w-auto  rounded-[1rem] max-md:rounded-[0.75rem] bg-white border border-stroke-100 dark:bg-cards-widget dark:border-stroke-40100',
         className,
       )}
     >
-      <div className="flex items-center justify-center gap-1.5 rounded-l-2xl border-r border-stroke-40100 px-4 py-3 ">
+      <div className="flex items-center justify-center gap-1.5 rounded-l-2xl border-r border-stroke-40100 px-4 py-3 max-md:px-3 max-md:py-2">
         <PointIcon className="size-4 text-[#7B61FF]" />
         <span className="text-sm font-medium leading-[1.5625rem] text-main-100">
-          {badgesInfo?.userRewards.totalPoints || 0}
+          {Math.round(badgesInfo?.userRewards.totalPoints || 0)}
         </span>
       </div>
 
       <button
-        className="flex items-center justify-center rounded-r-2xl bg-white px-3 transition-colors dark:border-stroke-40100 dark:bg-cards-widget"
+        className="flex items-center justify-center rounded-r-2xl bg-white px-3 transition-colors dark:border-stroke-40100 dark:bg-cards-widget max-md:px-2"
         type="button"
       >
         <PlusIcon className="size-3" />
